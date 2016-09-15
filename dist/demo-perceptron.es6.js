8c16a29fe5e1af9bdcc6b9ab6852294cfcb757ac
oleh.kastornov <powerbot15@gmail.com>
2016-09-15 17:28:24 +0300

    perceptron as single layer one neuron network

--- dist/demo-perceptron.es6.js
+++ dist/demo-perceptron.es6.js
@@ -1,11 +1,77 @@
 define(['neuron.es6'], function (_neuron) {
-  'use strict';
+    'use strict';
 
-  var perceptron = new _neuron.Neuron('symmetricalHardLimit', [0, 1, 0], 0);
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
 
-  console.log(perceptron.getNeuronOutput([1, -1, -1]) == -1 ? 'Orange' : 'Apple');
+    var _createClass = function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
 
-  console.log(perceptron.getNeuronOutput([1, 1, -1]) == -1 ? 'Orange' : 'Apple');
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
 
-  console.log(perceptron.getNeuronOutput([-1, -1, -1]) == -1 ? 'Orange' : 'Apple');
+    var Perceptron = function () {
+        function Perceptron() {
+            _classCallCheck(this, Perceptron);
+
+            this.weights = [0, 1, 0];
+
+            this.bias = 0;
+
+            this.transferType = 'symmetricalHardLimit';
+
+            this.initNet();
+        }
+
+        _createClass(Perceptron, [{
+            key: 'initNet',
+            value: function initNet() {
+
+                this.neuron = new _neuron.Neuron(this.transferType, this.weights, this.bias);
+            }
+        }, {
+            key: 'runNet',
+            value: function runNet(inputs) {
+
+                this.netResult = this.neuron.getNeuronOutput(inputs);
+            }
+        }, {
+            key: 'getResult',
+            value: function getResult() {
+
+                return this.netResult;
+            }
+        }]);
+
+        return Perceptron;
+    }();
+
+    var perceptron = new Perceptron();
+
+    perceptron.runNet([1, -1, -1]);
+
+    console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
+
+    perceptron.runNet([1, 1, -1]);
+
+    console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
+
+    perceptron.runNet([-1, -1, -1]);
+
+    console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
 });
--- src/demo-perceptron.es6
+++ src/demo-perceptron.es6
@@ -1,10 +1,50 @@
 
 import {Neuron} from 'neuron.es6'
 
-let perceptron = new Neuron('symmetricalHardLimit', [0, 1, 0], 0);
+class Perceptron{
 
-console.log(perceptron.getNeuronOutput([1, -1, -1]) == -1 ? 'Orange' : 'Apple');
+    constructor () {
 
-console.log(perceptron.getNeuronOutput([1, 1, -1]) == -1 ? 'Orange' : 'Apple');
+        this.weights = [0, 1, 0];
 
-console.log(perceptron.getNeuronOutput([-1, -1, -1]) == -1 ? 'Orange' : 'Apple');
+        this.bias = 0;
+
+        this.transferType = 'symmetricalHardLimit';
+
+        this.initNet();
+
+    }
+
+    initNet () {
+
+        this.neuron = new Neuron(this.transferType, this.weights, this.bias);
+
+    }
+
+    runNet (inputs) {
+
+        this.netResult = this.neuron.getNeuronOutput(inputs);
+
+    }
+
+    getResult () {
+
+        return this.netResult;
+
+    }
+
+}
+
+let perceptron = new Perceptron();
+
+perceptron.runNet([1, -1, -1]);
+
+console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
+
+perceptron.runNet([1, 1, -1]);
+
+console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
+
+perceptron.runNet([-1, -1, -1]);
+
+console.log(perceptron.getResult() == -1 ? 'Orange' : 'Apple');
