6e7a14f40fba9fd97a265d63513868119f7a86ed
oleh.kastornov <powerbot15@gmail.com>
2016-09-29 17:40:41 +0300

    optimization

--- gulpfile.js
+++ gulpfile.js
@@ -38,25 +38,25 @@ function compile(watch) {
         './src/app.es6'
     ], { debug: true }).transform(babel, {presets: ["es2015"]}));
 
-    function rebundle() {
-        bundler.bundle()
-            .on('error', function(err) { console.error(err); this.emit('end'); })
-            .pipe(source('build.js'))
-            .pipe(buffer())
-            //.pipe(sourcemaps.init({ loadMaps: true }))
-            //.pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest('build'));
-    }
-
     if (watch) {
         bundler.on('update', function() {
             console.log('-> bundling...');
-            rebundle();
+            rebundle(bundler);
             console.log('-> ready');
         });
     }
 
-    rebundle();
+    rebundle(bundler);
+}
+
+function rebundle(bundler) {
+    bundler.bundle()
+        .on('error', function(err) { console.error(err); this.emit('end'); })
+        .pipe(source('build.js'))
+        .pipe(buffer())
+        //.pipe(sourcemaps.init({ loadMaps: true }))
+        //.pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('build'));
 }
 
 function watch() {
